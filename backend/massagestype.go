b63ec57c820b36487df04ffc718b03006cc2081e
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-05-31 09:50:05 +0100

    chats new version

--- backend/massagestype.go
+++
@@ -1,9 +0,0 @@
-package backend
-
-type Message struct {
-	ID         int    `json:"id"`
-	SenderID   int    `json:"sender_id"`
-	ReceiverID int    `json:"receiver_id"`
-	Content    string `json:"content"`
-	Timestamp  string `json:"timestamp"`
-}
