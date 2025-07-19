c1bf5a8cbb3df18df9163e3e76a660557ca0154b
Your Name <you@example.com>
2025-07-19 05:54:35 +0100

    clean code

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"real-time-forum/backend"
 )
 
-func main()  {
+func main() {
 	var Server backend.Server
 	backend.MakeDataBase()
-	Server.Run()
+	Server.Run("8383")
 	Server.Shutdown()
 }
