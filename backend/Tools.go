0591099458c0f1ec344d021f1b008ae9ec53518c
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-08-19 21:37:18 +0100

    Revert "err page parte 1"
    This reverts commit d43e99f3759a4d65d3f1252024054d4c860aaab9.

--- backend/Tools.go
+++ backend/Tools.go
@@ -1,9 +1,7 @@
 package backend
 
 import (
-	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -14,9 +12,7 @@ type Error struct {
 }
 
 func renderErrorPage(w http.ResponseWriter, errMsg string, errCode int) {
-	w.WriteHeader(errCode)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Error{Err: errMsg, ErrNumber: strconv.Itoa(errCode)})
+	http.Error(w, errMsg, errCode)
 }
 
 func CheckPassword(hashedPassword, password string) error {
