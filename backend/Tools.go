ab7665539c76bdf2161da64ebdf9b968f578dffa
AniMaro <zkahlaou@gmail.com>
2025-08-24 00:23:00 +0100

    Fix typo in Paths array in checkHome function

--- backend/Tools.go
+++ backend/Tools.go
@@ -22,7 +22,7 @@ func CheckPassword(hashedPassword, password string) error {
 
 func checkHome(next http.Handler) http.Handler {
 
-	Paths := []string{"/app.js.", "/chat.js", "/comments.js", "error.js", "/login.js", "/logout.js", "/posts.js", "/regester.js", "/style.css", "/"}
+	Paths := []string{"/app.js", "/chat.js", "/comments.js", "/error.js", "/login.js", "/logout.js", "/posts.js", "/regester.js", "/style.css", "/"}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		for _, p := range Paths {
 			if r.URL.Path == p {
